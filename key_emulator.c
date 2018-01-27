636c55f6913f6c2c373a88a643f8957ee9f09ed4
Jeffersen <jeffersen42@gmail.com>
2018-01-27 12:08:41 +0100

    Simplify code

--- key_emulator.c
+++ key_emulator.c
@@ -9,15 +9,20 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 
+#define KEY_HOLD 2
 #define KEY_PRESS 1
 #define KEY_RELEASE 0
 #define BUFF_LENGTH 255
 
 static const char *uinput_file_path = "/dev/uinput";
+static char buff[BUFF_LENGTH] = "/dev/input/event";
+
+static char enabled = 1;
 static char shift_pressed = 0;
 static char ctrl_pressed = 0;
 
-static char buff[BUFF_LENGTH] = "/dev/input/event";
+int uinput_fd;
+int event_fd;
 
 const char* find_event_file(const char *file_name) {
     size_t len = 0;
@@ -47,16 +52,6 @@ const char* find_event_file(const char *file_name) {
 
     fclose(fd);
     return buff;
-}
-
-char is_shift(short code) {
-    return code == KEY_LEFTSHIFT ||
-           code == KEY_RIGHTSHIFT;
-}
-
-char is_ctrl(short code) {
-    return code == KEY_LEFTCTRL ||
-           code == KEY_RIGHTCTRL;
 }
 
 void emit(int fd, int code, int val) {
@@ -121,58 +116,147 @@ void press_while_ctrl_shift_key(int fd, int key) {
     emit(fd, KEY_LEFTSHIFT, 1);
 }
 
+struct event_handler {
+    // event value, event code
+    char (*check_condition)(int, int);
+    void (*execute)(void);
+};
+
+char is_shift_press(int value, int code) {
+    return ((code == KEY_LEFTSHIFT ||
+             code == KEY_RIGHTSHIFT) &&
+            (value == KEY_PRESS));
+}
+
+void execute_shift_press(void) {
+    shift_pressed = 1;
+}
+
+char is_shift_release(int value, int code) {
+    return ((code == KEY_LEFTSHIFT ||
+             code == KEY_RIGHTSHIFT) &&
+            (value == KEY_RELEASE));
+}
+
+void execute_shift_release(void) {
+    shift_pressed = 0;
+}
+
+char is_ctrl_press(int value, int code) {
+    return ((code == KEY_LEFTCTRL ||
+             code == KEY_RIGHTCTRL) &&
+            (value == KEY_PRESS));
+}
+
+void execute_ctrl_press(void) {
+    ctrl_pressed = 1;
+}
+
+char is_ctrl_release(int value, int code) {
+    return ((code == KEY_LEFTCTRL ||
+             code == KEY_RIGHTCTRL) &&
+            (value == KEY_RELEASE));
+}
+
+char is_enable(int value, int code) {
+    return (code == KEY_TAB &&
+             value == KEY_PRESS &&
+             ctrl_pressed == 1 &&
+             shift_pressed == 1);
+}
+
+void execute_enable(void) {
+    enabled = !enabled;
+}
+
+void execute_ctrl_release(void) {
+    ctrl_pressed = 0;
+}
+
+char is_page_up(int value, int code) {
+    if (value != KEY_PRESS && value != KEY_HOLD)
+        return 0;
+    return code == KEY_DELETE &&
+            enabled &&
+            shift_pressed &&
+            !ctrl_pressed;
+}
+
+void execute_page_up(void) {
+    press_while_shift_key(uinput_fd, KEY_PAGEUP);
+}
+
+char is_page_down(int value, int code) {
+    if (value != KEY_PRESS && value != KEY_HOLD)
+        return 0;
+    return code == KEY_PAUSE &&
+            enabled &&
+            shift_pressed &&
+            !ctrl_pressed;
+}
+
+void execute_page_down(void) {
+    press_while_shift_key(uinput_fd, KEY_PAGEDOWN);
+}
+
+char is_home(int value, int code) {
+    if (value != KEY_PRESS && value != KEY_HOLD)
+        return 0;
+    return code == KEY_PAUSE &&
+            enabled &&
+            shift_pressed &&
+            ctrl_pressed;
+}
+
+void execute_home(void) {
+    press_while_ctrl_shift_key(uinput_fd, KEY_HOME);
+}
+
+char is_end(int value, int code) {
+    if (value != KEY_PRESS && value != KEY_HOLD)
+        return 0;
+    return code == KEY_DELETE &&
+            enabled &&
+            shift_pressed &&
+            ctrl_pressed;
+}
+
+void execute_end(void) {
+    press_while_ctrl_shift_key(uinput_fd, KEY_END);
+}
+
+struct event_handler handles[] = {
+    {is_shift_press, execute_shift_press},
+    {is_shift_release, execute_shift_release},
+    {is_ctrl_press, execute_ctrl_press},
+    {is_ctrl_release, execute_ctrl_release},
+    {is_enable, execute_enable},
+    {is_page_up, execute_page_up},
+    {is_page_down, execute_page_down},
+    {is_home, execute_home},
+    {is_end, execute_end},
+};
+
 int main(int argc, char* argv[]) {
     const char *path = find_event_file("/proc/bus/input/devices");
-    int event_fd = open(path, O_RDONLY);
+    event_fd = open(path, O_RDONLY);
     if (event_fd == -1) {
         printf("Error event_fd\n");
         return -1;
     }
-    int uinput_fd = uinput_init(uinput_file_path);
+    uinput_fd = uinput_init(uinput_file_path);
     if (uinput_fd == -1) {
         printf("Error uinput_fd\n");
         return -1;
     }
-    printf("Sleep for device\n");
     sleep(3);
 
     struct input_event event;
     while (read(event_fd, &event, sizeof(struct input_event)) > 0) {
         if (event.type == EV_KEY) {
-            if (event.value == KEY_PRESS) {
-                if (is_shift(event.code)) {
-                    shift_pressed = 1;
-                }
-                if (is_ctrl(event.code)) {
-                    ctrl_pressed = 1;
-                }
-                if (event.code == KEY_DELETE &&
-                    shift_pressed == 1 &&
-                    ctrl_pressed == 1) {
-                    press_while_ctrl_shift_key(uinput_fd, KEY_END);
-                }
-                if (event.code == KEY_DELETE &&
-                    shift_pressed == 1 &&
-                    ctrl_pressed == 0) {
-                    press_while_shift_key(uinput_fd, KEY_PAGEUP);
-                }
-                if (event.code == KEY_PAUSE &&
-                    shift_pressed == 1 &&
-                    ctrl_pressed == 1) {
-                    press_while_ctrl_shift_key(uinput_fd, KEY_HOME);
-                }
-                if (event.code == KEY_PAUSE &&
-                    shift_pressed == 1 &&
-                    ctrl_pressed == 0) {
-                    press_while_shift_key(uinput_fd, KEY_PAGEDOWN);
-                }
-            }
-            if (event.value == KEY_RELEASE) {
-                if (is_shift(event.code)) {
-                    shift_pressed = 0;
-                }
-                if (is_ctrl(event.code)) {
-                    ctrl_pressed = 0;
+            for(int i = 0; i < sizeof(handles) / sizeof(struct event_handler); i++){
+                if (handles[i].check_condition(event.value, event.code)) {
+                    handles[i].execute();
                 }
             }
         }
